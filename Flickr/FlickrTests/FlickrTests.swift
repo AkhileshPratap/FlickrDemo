97c9479b1ba5e0b9404d5303bedc8df46ea8e2ba
Akhilesh Pratap Singh <jss.akhileshpratap@gmail.com>
2021-07-16 13:59:53 +0530

    [Task]: unit test coverage for photo view model class

--- Flickr/FlickrTests/FlickrTests.swift
+++ Flickr/FlickrTests/FlickrTests.swift
@@ -10,24 +10,60 @@ import XCTest
 
 class FlickrTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let assembler = Assembler()
+    var viewModel: PhotosViewModel?
+    var photos: [Photo]?
+    var service: APIServiceProtocol?
+
+    override func setUp() {
+        super.setUp()
+        viewModel = assembler.viewModel()
+        service = assembler.apiService()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testCheckIfDataReceivedFromServer() {
+        var isSuccess = false
+        let expect = expectation(description: "check network call")
+        viewModel?.getPhotos { (result) in
+            switch result {
+            case .success(_):
+                isSuccess = true
+            case .failure(_):
+                isSuccess = false
+            }
+            XCTAssert(isSuccess == true, "server data received")
+            expect.fulfill()
         }
+        waitForExpectations(timeout: 5) { error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
     }
 
+    func testGetPhotos() {
+        let expect = expectation(description: "get data from api")
+        service?.getPhotos { [weak self](result) in
+            switch result {
+            case .success(let model):
+                self?.photos = model?.photos?.photo ?? []
+            case .failure(_):
+                break
+            }
+            XCTAssert((self?.photos!.count)! > 0, "photos received")
+            expect.fulfill()
+        }
+
+        waitForExpectations(timeout: 5) { error in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+    }
+
+    func testNumberOfRows() {
+        testGetPhotos()
+        XCTAssertEqual(self.photos!.count, 20)
+    }
+    
 }
